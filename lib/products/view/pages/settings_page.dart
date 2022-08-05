9dace30c165e14cdd96c398e506cb7de3f0509d0
bath0ry <plylegacy@gmail.com>
2022-08-05 02:16:18 -0300

    icons

--- lib/products/view/pages/settings_page.dart
+++ lib/products/view/pages/settings_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_shop_app/home/view/pages/home_page.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 30, top: 80, right: 10, bottom: 0),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(
+                        MaterialPageRoute(builder: (context) => HomePage()));
+                  },
+                  icon: Icon(Icons.arrow_back_ios))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
